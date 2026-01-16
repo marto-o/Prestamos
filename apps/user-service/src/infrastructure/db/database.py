4f8347f461de477451a20c28e709e617b5853496
marto-o <marto.sel02@gmail.com>
2026-01-16 14:35:07 -0300

    .

--- apps/user-service/src/infrastructure/db/database.py
+++ apps/user-service/src/infrastructure/db/database.py
@@ -3,10 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://user_admin:PCt7wo4!@localhost:5432/users_db"
 
-# El 'engine' es el encargado de la conexión física
 engine = create_engine(DATABASE_URL)
 
-# 'SessionLocal' es lo que usaremos para hacer consultas (SELECT, INSERT, etc)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
