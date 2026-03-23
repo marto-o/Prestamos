5b446b60ba4da46cd82f40978637a8c9d72c9861
MartinEspinozaLedezma <mespinoza@usm.cl>
2026-03-23 15:15:31 -0300

    se quitan lineas que reemplazan script del readme

--- apps/loan-service/src/infrastructure/database.py
+++ apps/loan-service/src/infrastructure/database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
 
 DATABASE_URL = "postgresql://user_admin:PCt7wo4!@localhost:5433/loans_db"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
